b0adc186254d19bc3668749d273f910bad0907c7
Abdur Rehman <abdurrehmanlkl786@gmail.com>
2022-02-18 21:28:11 +0500

    write first program in python

---
+++ first_program.py
@@ -0,0 +1,9 @@
+# firstName = input("Enter your first name: ")
+# secondeName = input('enter your seconde name: ')
+# print('your full name is: ' + firstName + secondeName )
+
+print('20 days have ' + str(300) + ' minutes')
+# if is used for format, 
+print(f'18 days have {18 * 24 * 60}  mintutes') 
+age = 23 + 2;
+print(f'age is: {age}');
---
+++ inputs_user.py
@@ -0,0 +1,5 @@
+# from tkinter.tix import InputOnly
+
+value1 = input('Enter first number: ');
+value2 = input('Enter second number: ');
+result = print('your answer is: {value1 + value2}')
