a88703ef6c2d09d7773bfb67a2bcc3b12bed48a8
Abdur Rehman <abdurrehmanlkl786@gmail.com>
2022-02-19 01:13:24 +0500

    scope of variables

--- functions.py
+++ functions.py
@@ -3,18 +3,22 @@
 calculate_seconds =  24 * 60 * 60;
 time_unit = 'minutes'
 
-
+#function definition and its body
 def time_calculation(days):
     print(f'{days} days have {days * calculate_seconds} {time_unit}')
-   
 
+def scope_check(days, message) :
+    print(days)  
+    print(message) 
 
+#Call to function
 time_calculation(20);
 time_calculation(21);
-
-time_calculation(22);
 time_calculation(200);
 time_calculation(2.51);
+
+#call to scope function, where we just checking the scop of variable
+scope_check(180, 'variable scope')
 
 
 
