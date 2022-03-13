df11120e6a738d40e9793e05c70d50a541b1c0bc
Abdur Rehman <abdurrehmanlkl786@gmail.com>
2022-03-13 11:44:04 +0500

    importing libraries

---
+++ python_basics/import_libraries.py
@@ -0,0 +1,15 @@
+
+# Libraries are the large functions or classes that are written
+# by other programmers and we just call it into our program
+# in order to use it is functionality or some part of it.
+
+# import math library to do math operation
+
+import math
+print('the value of pi is: ', math.pi)
+print(math.sqrt(9))
+
+# import statistic library to use basic statistic operations
+import statistics
+a = [100, 200, 300,4, 5]
+print(statistics.median(a))
